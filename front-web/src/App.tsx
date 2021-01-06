5859a49a7e51613ee10f9ea739fa08b335271f8f
Geydel Valfre Marozzi <geydel.marozzi@accenture.com>
2021-01-06 19:11:53 -0300

    Front-Web: Add NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
-import Counter from './Counter';
+import Navbar from './Navibar';
 
 function App() {
   return (
     <div>
-      <Counter />
+      <Navbar />
     </div>
   );
 }
