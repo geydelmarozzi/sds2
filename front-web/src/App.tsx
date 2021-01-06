ccb0e017db05a9f0ddbb29e1360a4b3613d77f2a
Geydel Valfre Marozzi <geydel.marozzi@accenture.com>
2021-01-06 19:29:36 -0300

    Front-Web: Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from './Navibar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
